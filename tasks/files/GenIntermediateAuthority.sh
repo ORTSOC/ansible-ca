83bb1a33b18a3a5d0987df94f6f1b5bdff8347db
greenwac <greenwac@oregonstate.edu>
2021-02-19 17:24:36 -0800

    Add playbook for CA distribution

--- tasks/files/GenIntermediateAuthority.sh
+++ tasks/files/GenIntermediateAuthority.sh
@@ -15,7 +15,3 @@ echo ""
 openssl req -new -key /ca-stuff/IntermediateCA.key -subj "/C=US/ST=Oregon/L=Corvallis/O=ORTSOC/CN=int_auth" -out /ca-stuff/IntermediateCA.csr
 
 
-echo "Signing Intermediate Authority CSR with Root Authority"
-openssl x509 -req -days 730 -in /ca-stuff/IntermediateCA.csr -CA /ca-stuff/RootCA.crt -CAkey /ca-stuff/RootCA.key  -CAcreateserial -out /ca-stuff/IntermediateCA.crt
-
-
