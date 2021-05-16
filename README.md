7aebdc6476eebc02381c648e16554ec5f4359dd5
greenwac <greenwac@oregonstate.edu>
2021-05-16 13:07:27 -0700

    change CN to user input and add SAN field

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Deploys OpenSSL, a certificate authority.
 
 To generate a new standard or p12 certificate:
 	* login to the int authority as ortsoc-admin
-	* run /ca-stuff/GenerateCertificate.sh [name] or/ca-stuff/GenerateP12Cert.sh [name]
+	* run /ca-stuff/GenerateCertificate.sh [name] [CN] [SAN] or/ca-stuff/GenerateP12Cert.sh [name] [CN] [SAN]
 	* Both will require a passphrase when generating the RSA key
 	* To decrypt the RSA key run: openssl rsa -in [keyfile.key]
 
--- files/GenerateCertificate.sh
+++ files/GenerateCertificate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ];
-	then echo "Usage: $0 [certificate name]"
+if [ "$#" -ne 3 ];
+	then echo "Usage: $0 [certificate name] [CN] [SAN]"
 fi
 
 echo "Generating New Certificate signed by Intermediate CA" 
@@ -12,7 +12,7 @@ serial=`date +%s`
 
 openssl genrsa -aes256 -out /ca-stuff/"$1".key 2048
 
-openssl req -new -key /ca-stuff/"$1".key -subj "/C=US/ST=Oregon/L=Corvallis/O=ORTSOC/CN=$1_cert" -out /ca-stuff/"$1".csr
+openssl req -new -key /ca-stuff/"$1".key -subj "/C=US/ST=Oregon/L=Corvallis/O=ORTSOC/CN=$2/subjectAltName=$3" -out /ca-stuff/"$1".csr
 
 openssl x509 -req -in /ca-stuff/"$1".csr -CA /ca-stuff/IntermediateCA.crt -CAkey /ca-stuff/IntermediateCA.key -out /ca-stuff/"$1".crt -days 730 -set_serial $serial 
 
