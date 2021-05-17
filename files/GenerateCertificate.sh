645f77667c38c08c295aeadda9a8bc7fcd62e0a0
greenwac <greenwac@oregonstate.edu>
2021-05-16 17:21:37 -0700

    made SANs optional

--- files/GenerateCertificate.sh
+++ files/GenerateCertificate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ];
+if [ "$#" -le 1 ];
 	then echo "Usage: $0 [certificate name] [CN] [SAN]"
 fi
 
@@ -12,7 +12,14 @@ serial=`date +%s`
 
 openssl genrsa -aes256 -out /ca-stuff/"$1".key 2048
 
+
+if [ "$#" -eq 3 ]; then
 openssl req -new -key /ca-stuff/"$1".key -subj "/C=US/ST=Oregon/L=Corvallis/O=ORTSOC/CN=$2/subjectAltName=$3" -out /ca-stuff/"$1".csr
+fi
+
+if [ "$#" -eq 2 ]; then
+openssl req -new -key /ca-stuff/"$1".key -subj "/C=US/ST=Oregon/L=Corvallis/O=ORTSOC/CN=$2" -out /ca-stuff/"$1".csr
+fi
 
 openssl x509 -req -in /ca-stuff/"$1".csr -CA /ca-stuff/IntermediateCA.crt -CAkey /ca-stuff/IntermediateCA.key -out /ca-stuff/"$1".crt -days 730 -set_serial $serial 
 
